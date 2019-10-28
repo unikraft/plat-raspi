b4a3580e640d2f122fbd619d0a5c0161b5d00b99
Santiago Pagani <santiago.pagani@neclab.eu>
2019-10-28 15:57:51 +0100

    UK ALLOC and UK SCHED working

--- memory.c
+++ memory.c
@@ -1,15 +1,104 @@
 
 #include <uk/plat/memory.h>
+#include <sections.h>
 #include <uk/essentials.h>
 
 int ukplat_memregion_count(void)
 {
-	return 0;
+	return 7;
 }
 
-int ukplat_memregion_get(int i __unused, struct ukplat_memregion_desc *m __unused)
+int ukplat_memregion_get(int i, struct ukplat_memregion_desc *m)
 {
-	return 0;
+	int ret;
+
+	UK_ASSERT(m);
+
+	switch (i) {
+	case 0: /* stack */
+		m->base  = (void *) 0;
+		m->len   = (size_t) __TEXT;
+		m->flags = (UKPLAT_MEMRF_RESERVED
+			    | UKPLAT_MEMRF_READABLE
+			    | UKPLAT_MEMRF_WRITABLE);
+		ret = 0;
+#if CONFIG_UKPLAT_MEMRNAME
+		m->name  = "stack";
+#endif
+		break;
+	case 1: /* text */
+		m->base  = (void *) __TEXT;
+		m->len   = (size_t) __ETEXT - (size_t) __TEXT;
+		m->flags = (UKPLAT_MEMRF_RESERVED
+			    | UKPLAT_MEMRF_READABLE);
+#if CONFIG_UKPLAT_MEMRNAME
+		m->name  = "text";
+#endif
+		ret = 0;
+		break;
+	case 2: /* rodata */
+		m->base  = (void *) __RODATA;
+		m->len   = (size_t) __ERODATA - (size_t) __RODATA;
+		m->flags = (UKPLAT_MEMRF_RESERVED
+			    | UKPLAT_MEMRF_READABLE);
+#if CONFIG_UKPLAT_MEMRNAME
+		m->name  = "rodata";
+#endif
+		ret = 0;
+		break;
+	case 3: /* ctors */
+		m->base  = (void *) __CTORS;
+		m->len   = (size_t) __ECTORS - (size_t) __CTORS;
+		m->flags = (UKPLAT_MEMRF_RESERVED
+			    | UKPLAT_MEMRF_READABLE);
+#if CONFIG_UKPLAT_MEMRNAME
+		m->name  = "ctors";
+#endif
+		ret = 0;
+		break;
+	case 4: /* data */
+		m->base  = (void *) __DATA;
+		m->len   = (size_t) __EDATA - (size_t) __DATA;
+		m->flags = (UKPLAT_MEMRF_RESERVED
+			    | UKPLAT_MEMRF_READABLE
+			    | UKPLAT_MEMRF_WRITABLE);
+#if CONFIG_UKPLAT_MEMRNAME
+		m->name  = "data";
+#endif
+		ret = 0;
+		break;
+	case 5: /* bss */
+		m->base  = (void *) __BSS_START;
+		m->len   = (size_t) __BSS_END - (size_t) __BSS_START;
+		m->flags = (UKPLAT_MEMRF_RESERVED
+			    | UKPLAT_MEMRF_READABLE
+			    | UKPLAT_MEMRF_WRITABLE);
+#if CONFIG_UKPLAT_MEMRNAME
+		m->name  = "bss";
+#endif
+		ret = 0;
+		break;
+	case 6: /* heap */
+		m->base  = (void *) __HEAP_START;
+		m->len   = (size_t) __HEAP_END - (size_t) __HEAP_START;
+		m->flags = UKPLAT_MEMRF_ALLOCATABLE;
+#if CONFIG_UKPLAT_MEMRNAME
+		m->name  = "heap";
+#endif
+		ret = 0;
+		break;
+	default:
+		m->base  = __NULL;
+		m->len   = 0;
+		m->flags = 0x0;
+#if CONFIG_UKPLAT_MEMRNAME
+		m->name  = __NULL;
+#endif
+		ret = -1;
+		break;
+	}
+
+	return ret;
 }
 
 int _ukplat_mem_mappings_init(void)
